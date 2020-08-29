649e997a32071261867280451d41cbda7abc4e62
LIASY032 <liasy032@mymail.unisa.edu.au>
2020-08-29 22:02:52 +0930

    fixed the Item class

--- item.cpp
+++ item.cpp
@@ -2,9 +2,27 @@
 
 #include <iostream>
 
-Item::Item()
-{
+Weapon::Weapon(std::string name) : _name{name}{
 
+}
 
+Item* Weapon::clone() {
 
+    return new Weapon;
+}
+
+std::string Weapon::name(){
+    return _name;
+}
+
+Consumable::Consumable(std::string name): _name{name}{
+
+}
+
+Item* Consumable::clone() {
+    return new Consumable;
+}
+
+std::string Consumable::name(){
+    return _name;
 }
--- item.h
+++ item.h
@@ -2,21 +2,41 @@
 #define ITEM_H
 #include <iostream>
 
+
+
+
 class Item
 {
 private:
     std::string _name;
 public:
+    Item() = default;
     Item(std::string name);
-    ~Item() = default;
-    virtual Item clone();
+    virtual ~Item() = default;
+    virtual Item* clone() = 0;
     virtual std::string name() = 0;
 
 };
 
-
 class Weapon : public Item{
+public:
+    Weapon() = default;
+    Weapon(std::string name);
+    Item* clone() override;
+    std::string name() override;
+private:
+    std::string _name;
+};
 
+
+class Consumable: public Item{
+public:
+    Consumable() = default;
+    Consumable(std::string name);
+    Item* clone() override;
+    std::string name() override;
+private:
+    std::string _name;
 };
 
 
